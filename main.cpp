6c0f91b17bb740e8e04e666c3a3fd0aa3d8ab808
Abhinandan Sah <abhinandansah222@gmail.com>
2023-06-16 14:41:12 +0545

    This is c++ commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+void bubblesort{
+	//this is bubblesort
+
+void quicksort{
+	//this is quicksort
+
+}
+
+void insertionsort{
+
+}
+
+void mergesort{
+
+}
+
+int main(){
+	cout<<"hello world";
+	int a=1;
+	cout<<a<<endl;
+	return 0;
+}
